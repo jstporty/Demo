5a3871c1a37fe3c7da5554cf16cfa2cd28407ec8
승현 <horang@mz.co.kr>
2024-01-16 14:55:54 +0900

    의존 관계 자동 주입

--- src/main/java/com/hello/core/order/service/OrderServiceImpl.java
+++ src/main/java/com/hello/core/order/service/OrderServiceImpl.java
@@ -1,26 +1,39 @@
 package com.hello.core.order.service;
 
+import com.hello.core.annotation.MainDiscountPolicy;
 import com.hello.core.discount.DiscountPolicy;
 import com.hello.core.member.Member;
 import com.hello.core.member.repository.MemberRepository;
 import com.hello.core.order.Order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     //private final DiscountPolicy discountPolicy = new FixDiscountPloicy();
-    private final  MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy ;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy
-            discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository,
+                            @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    //수정자
+//수정자 자동 주입
+    @Autowired
+    public DiscountPolicy setDiscountPolicy(@MainDiscountPolicy DiscountPolicy discountPolicy) {
+        return this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
